d6d7892617e5fa4d24ee98e456a049c31ace1901
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-03-20 15:00:29 -0300

    aplicando filtro para buscar frases na lista

--- app/src/main/java/com/example/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.motivation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.example.motivation.infrastructure.MotivationConstants
 import com.example.motivation.R
+import com.example.motivation.data.Mock
 import com.example.motivation.infrastructure.SecurityPreferences
 import com.example.motivation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var categoryId = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,19 +26,53 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         supportActionBar?.hide()
 
         handleUserName()
+        handleFilter(R.id.image_all)
+        handleNextPhrase()
 
         // Eventos
         binding.buttonNewPhase.setOnClickListener(this)
+
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_phase) {
-
+            handleNextPhrase()
+        } else if (view.id in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)) {
+            handleFilter(view.id)
         }
     }
 
     private fun handleUserName() {
         val name = SecurityPreferences(this).getString(MotivationConstants.KEY.USER_NAME)
         binding.textUserName.text = "Olá, $name!"
+    }
+
+    private fun handleFilter(id: Int) {
+
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+
+        when (id) {
+            R.id.image_all -> {
+                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.ALL
+            }
+            R.id.image_happy -> {
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.HAPPY
+            }
+            R.id.image_sunny -> {
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.SUNNY
+            }
+        }
+    }
+
+    private fun handleNextPhrase() {
+        binding.textPhrase.text = Mock().getPhase(categoryId)
     }
 }
